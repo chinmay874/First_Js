1c1a2e37ec0ccea26607d27cb38cd2f352ffc630
chinmay jain <43497155+chinmay874@users.noreply.github.com>
2020-06-16 09:10:52 +0530

    update

--- goodbye.js
+++ goodbye.js
@@ -1,1 +1,8 @@
-
+(function(window){
+  var byeS = new object();
+  var speakW="Good Bye";
+  byeS.speak= function speak(name){
+    console.log(speakW + " " + name);
+  };
+  window.byeS = byeS;
+})(window);
