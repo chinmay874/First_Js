62f5c1d0a0ca037a12e289c4b08fa6ef801b98ef
chinmay jain <43497155+chinmay874@users.noreply.github.com>
2020-06-16 09:05:51 +0530

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-
+(function(window){
+  var helloS= new Object();
+  var speakW="Hello";
+  helloS.speak= function(name){
+    console.log(speakW + " " +name);
+  };
+  window.helloS = helloS;
+})(window);
