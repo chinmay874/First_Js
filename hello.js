4c39516731df134036b9e0d1f227556a355f5c06
chinmay jain <43497155+chinmay874@users.noreply.github.com>
2020-06-16 22:10:01 +0530

    delete

--- hello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-  var helloS= new Object();
-  var speakW="Hello";
-  helloS.speak= function(name){
-    console.log(speakW + " " +name);
-  };
-  window.helloS = helloS;
-})(window);
